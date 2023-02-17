24b58016371ab9ebb7691de013d04a143d16ce48
Josue Hernandez <josuehdz0@gmail.com>
2023-02-17 14:07:33 -0700

    fixed env

--- client/app/env.js
+++ client/app/env.js
@@ -3,8 +3,5 @@ export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 export const domain = 'swanky.us.auth0.com'
 export const audience = 'https://swanky.com'
-<<<<<<< HEAD
 export const clientId = 'yrk8rIXdLCdPxwEJTIKeOyeczNqZXx2R'
-=======
-export const clientId = 'yrk8rIXdLCdPxwEJTIKeOyeczNqZXx2R'
->>>>>>> 3ecfac840902c308b75588006766a25391c68821
+
