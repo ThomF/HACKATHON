00f56f1398a78f81c2d1d5bfaf44e9db39024403
ThomF <seniorswanky@icloud.com>
2023-02-17 12:45:06 -0700

    back - dbcontext

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { VinylSchema } from "../models/Vinyl.js";
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
