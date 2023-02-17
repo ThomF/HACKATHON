eb2c23dbefbbdfaaf8ec0ad3af4f46bb8e1a8efe
Josue Hernandez <josuehdz0@gmail.com>
2023-02-17 15:55:05 -0700

    Added template for vinylpost

--- client/app/Models/Vinyl.js
+++ client/app/Models/Vinyl.js
@@ -9,5 +9,23 @@ export class Vinyl {
         this.description = data.description
     }
 
-    
+    get vinylTemplate(){
+        return`
+        <div class="col-8 my-2">
+          <div class="row mycard">
+            <div class="col-5">
+              <div class="img-fluid d-flex align-items-center">
+                <img class="incard" src="${this.albumCover}" alt="picture">
+              </div>
+            </div>
+            <div class="col-7">
+              <div class="">
+                <h5>${this.title}</h5>
+                <h6>${this.description}</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        `
+    }
 }
