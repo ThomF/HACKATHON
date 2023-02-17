f951e0cb81bb07a68cd69d61b9bef667529bdd83
Dustin Bates <dustin.bates98@gmail.com>
2023-02-17 15:59:17 -0700

    front end get and create added

--- client/app/Controllers/VinylsController.js
+++ client/app/Controllers/VinylsController.js
@@ -1,10 +1,46 @@
+import { appState } from "../AppState.js";
+import { vinylsService } from "../Services/VinylsService.js";
+import { getFormData } from "../Utils/FormHandler.js";
+import { Pop } from "../Utils/Pop.js";
+import { setHTML } from "../Utils/Writer.js";
+
+function _drawVinyls(){
+    let template = ''
+    appState.vinyls.forEach(vinyl => template += vinyl.vinylTemplate)
+    setHTML('vinyls', template);
+}
+
 export class VinylsController{
-    
+
 
     constructor(){
         console.log("Hello from the Vinyls Controller")
+        this.getVinyls();
+        appState.on('vinyls', _drawVinyls)
 
     }
 
+    async postVinyl(){
+        try {
+            window.event?.preventDefault();
+            let form = window.event.target;
+            let vinylBody = getFormData(form)
+            console.log("Playing Vinyl", vinylBody)
+            await vinylsService.postVinyl(vinylBody);
+        } catch (error) {
+            console.error(error);
+            Pop.error(error.message)
+        }
+    }
 
+    async getVinyls(){
+        try {
+            let vinyls = await vinylsService.getVinyls();
+
+        } catch (error) {
+            console.error(error)
+            // @ts-ignore
+            Pop.error("Error", error.message)
+        }
+    }
 }
--- client/app/Services/VinylsService.js
+++ client/app/Services/VinylsService.js
@@ -1,4 +1,19 @@
+import { appState } from "../AppState.js";
+import { Vinyl } from "../Models/Vinyl.js";
+import { server } from "./AxiosService.js";
+
 class VinylsService{
+    async postVinyl(vinylBody) {
+        const res = await server.post('api/vinyls', vinylBody)
+        console.log("Playing Vinyl", res.data);
+    }
+
+    async getVinyls() {
+        const res = await server.get('api/vinyls');
+        console.log("Getting Vinyls", res.data);
+        appState.vinyls = res.data.map(v => new Vinyl(v))
+        console.log("Got Vinyls", appState.vinyls)
+    }
 
 }
 
