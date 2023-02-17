48d2999a3f265f3c26b892984ab809e296a581ed
ThomF <seniorswanky@icloud.com>
2023-02-17 15:29:18 -0700

    back - find vinyl id

--- server/services/VinylsServices.js
+++ server/services/VinylsServices.js
@@ -1,7 +1,16 @@
 import { dbContext } from "../db/DbContext"
-
+import { BadRequest } from "../utils/Errors.js"
 
 class VinylsServices {
+    async getVinylById(vinylId) {
+        const vinyl = await dbContext.Vinyls.findById(vinylId)
+            .populate('creator', 'name picture')
+            .populate('watchCount')
+        if (!vinyl) {
+            throw new BadRequest('Bad Vinyl ID')
+        }
+        return vinyl
+    }
     async createVinyl(vinylData) {
         const newVinyl = await dbContext.Vinyls.create(vinylData)
         // await newVinyl.populate('')
