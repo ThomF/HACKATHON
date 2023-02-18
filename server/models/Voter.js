bf39b21555e69c257e42c32daac4d62de3301ce8
Scott Tippetts <wstippetts+github@gmail.com>
2023-02-18 11:21:03 -0700

    BE - fixed voter logic

--- server/models/Voter.js
+++ server/models/Voter.js
@@ -4,9 +4,12 @@ import { Schema } from "mongoose";
 export const VoterSchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
-    upVote: { type: Boolean, default: false },
+    upVoteComment: { type: Schema.Types.ObjectId, ref: "Comments" },
+    upVoteVinyl: { type: Schema.Types.ObjectId, ref: "Vinyl" },
     //downVote:{},
-  }
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+
 )
 
 //**** VIRTUAL PROPERTIES ****/
@@ -25,3 +28,6 @@ VoterSchema.virtual('vinylVoter', {
   ref: 'Vinyls'
 }
 )
+
+VoterSchema.index({ userId: 1, upVoteComment: 1 }, { unique: true })
+VoterSchema.index({ userId: 1, upVoteVinyl: 1 }, { unique: true })
